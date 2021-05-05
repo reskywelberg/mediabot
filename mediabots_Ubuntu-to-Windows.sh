fb5481319cca0d4bc39aca13442627d763a935bc
Nur Reski Mustamin <62367250+reskywelberg@users.noreply.github.com>
2021-05-05 23:59:00 +0700

    Update mediabots_Ubuntu-to-Windows.sh

--- mediabots_Ubuntu-to-Windows.sh
+++ mediabots_Ubuntu-to-Windows.sh
@@ -36,9 +36,9 @@ link1_status=$(curl -Is http://download.microsoft.com/download/6/2/A/62A76ABB-99
 link2_status=$(curl -Is https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://archive.org/download/WS2012R2/WS2012R2.ISO # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/WS2012R2.ISO https://dl.dropbox.com/s/vqerlw3mnwlusap/Windows10_1909.iso?dl=0
+	sudo wget -O /mediabots/WS2012R2.ISO https://bit.ly/2Abltln
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://dl.dropbox.com/s/vqerlw3mnwlusap/Windows10_1909.iso?dl=0
+	sudo wget -P /mediabots https://bit.ly/2Abltln
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
